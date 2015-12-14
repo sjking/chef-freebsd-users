4b3d01558a3efc8e7ca56ba627a92ad75ab2374c
sjking <stevejohnking@gmail.com>
2015-12-13 23:31:16 -0800

    add vim recipe

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -1,21 +1,21 @@
 #
 # Cookbook Name:: chef-freebsd-users
-# Recipe:: default
+# Recipe:: vim
 #
 # The MIT License (MIT)
-# 
+#
 # Copyright (c) 2015 Steve King
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,3 +24,20 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+package 'vim-lite'
+
+# set the .vimrc for each user
+users = data_bag(node['chef_freebsd_users']['data_bag-dir'])
+
+users.each do |login|
+  user = data_bag_item('users', login)
+  home_dir = user['home']
+
+  cookbook_file "#{home_dir}/.vimrc" do
+    source node['chef_freebsd_users']['vim']['vimrc_file']
+    owner user['id']
+    group user['id']
+    mode '0644'
+    action :create
+  end
+end
